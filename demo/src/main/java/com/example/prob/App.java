fa4c6209185798f22650a92bce8deb917b0f1186
AntonOrduna <anton.orduna@edu.xunta.gal>
2025-10-20 17:06:42 +0200

    nuevo commit

--- demo/src/main/java/com/example/prob/App.java
+++ demo/src/main/java/com/example/prob/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("¡Hola desde Java en VS Code!");
         System.out.println("Nueva línea desde el editor de GIthub");
+        System.out.println("Nuevas modificaciones");
     }
 }
